1288c0b9f47234d604e4d4e91adc5832a493ada5
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-07-29 20:09:31 +0530

    Add files via upload

---
+++ Reverse String II Question (L-541).java
@@ -0,0 +1,16 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+        for(int i =0; i <s.length();i+=2*k){
+            int start = i,
+            end = Math.min(i+k-1, s.length()-1);
+            while(start < end){
+                char temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;end--;
+            }
+        }
+        return new String(arr);
+    }
+}
